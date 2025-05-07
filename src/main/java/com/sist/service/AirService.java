8feb46c8402a6a4fd78af5953051a56240ef448b
bwb <bwb@Window>
2025-05-07 18:48:49 +0900

    2025-05-07_flightInfoVO_오류문제로 재commit

--- src/main/java/com/sist/service/AirService.java
+++ src/main/java/com/sist/service/AirService.java
@@ -2,7 +2,7 @@ package com.sist.service;
 
 import java.util.List;
 import com.sist.vo.air.*;
-
+ 
 public interface AirService {
 	public List<FlightInfoVO> flightListData(int start,int end);
 	public int flightTotalPage();
